4af24ebb28dfad9478b7b2c17f97b6201eb5e197
KanzakiMoe <haruyukishizuku@outlook.com>
2024-10-16 18:09:15 +0800

    Add GUI and change the output path

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from tkinter.filedialog import askopenfilename
+import tkinter as tk
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter.filedialog import askopenfilename
 from sklearn.linear_model import LinearRegression
 
 # Draw linear regression function
@@ -38,8 +39,6 @@ def plot_linear_regression(x,y1,y2):
     [label.set_fontname('Arial') for label in labels]
     plt.tick_params(axis='both',width=1,length=5)
     plt.draw()
-    print("[nitrite]:F(x)=",lineModel1.coef_[0][0],"x + ",lineModel1.intercept_[0])
-    print("[nitrate]:F(x)=",lineModel2.coef_[0][0],"x + ",lineModel2.intercept_[0])
     parameters = [lineModel1.coef_[0][0],lineModel1.intercept_[0],lineModel2.coef_[0][0],lineModel2.intercept_[0]]
     return parameters
 
@@ -48,10 +47,10 @@ def Nitrite_N(data,parameters,lower_limit,higher_limit):
     Nitrite_N_result = []
     for row in data:
         if row[1] > lower_limit:
-            if row[1] > higher_limit:
-                nitrogen = "overlimit"
-                Nitrite_N_result.append(nitrogen)
-            else:
+            # if row[1] > higher_limit:
+            #     nitrogen = "overlimit"
+            #     Nitrite_N_result.append(nitrogen)
+            # else:
                 nitrogen = (row[1]+parameters[1])/parameters[0]*float(row[0][-3:-1])/46.005*14.007
                 Nitrite_N_result.append(nitrogen)
         else:
@@ -65,10 +64,10 @@ def Nitrate_N(data,parameters,lower_limit,higher_limit):
     Nitrate_N_result = []
     for row in data:
         if row[2] > lower_limit:
-            if row[2] > higher_limit:
-                nitrogen = "overlimit"
-                Nitrate_N_result.append(nitrogen)
-            else:
+            # if row[2] > higher_limit:
+            #     nitrogen = "overlimit"
+            #     Nitrate_N_result.append(nitrogen)
+            # else:
                 nitrogen = (row[2]+parameters[3])/parameters[2]*float(row[0][-3:-1])/62.004*14.007
                 Nitrate_N_result.append(nitrogen)
         else:
@@ -78,12 +77,58 @@ def Nitrate_N(data,parameters,lower_limit,higher_limit):
     return NO3N
 
 def main():
-    # Enter user's name which is marked on data label
-    username = input("Please enter your name, which is same as the data label: ")
     
-    # Choose .csv file and read data from it
-    print("Please choose the data file with ending of .csv")
-    data_path = askopenfilename()
+    
+    '''
+    GUI packing start
+    '''
+    
+    # Window setting
+    root = tk.Tk()
+    root.title("MagIC data processing")
+    root.geometry("800x300")
+    
+    # Username part
+    username_frame = tk.Frame(root)
+    username_frame.grid(row=0)
+    
+    ## Username label
+    username_requiring_text = tk.Label(username_frame, text="Please enter your name, which is same as the data label:")
+    username_requiring_text.grid(row=0, column=0)
+    
+    ## Username input box and button
+    username_value = tk.Entry(username_frame)
+    username_value.grid(row=0, column=1)
+    def printget(): # Get username data from input box to global variable "username" and return confirm message
+        global username
+        username = username_value.get()
+        username_confirm = tk.Label(username_frame, text="Confirm your name: " + username)
+        username_confirm.grid(row=0, column=4)
+    tk.Button(username_frame, text = "Confirm", command = lambda: printget()).grid(row=0, column=2)
+    
+    # Data extraction
+    data_frame = tk.Frame(root)
+    data_frame.grid(row=1)
+    
+    ## Data file choosing button
+    def openfile(username): # Get exported data from filemanager to global variable "data_path" and return confirm message
+        global data_path
+        data_path = askopenfilename(title = "Please choose the export file", filetypes=[("MagIC output file", "*.csv")], defaultextension=".csv")
+        data_confirm = tk.Label(data_frame, text="Confirm your data path: " + data_path)
+        data_confirm.grid(row=0,column=1)
+        # Generate confirm button, which is to continue the calculation part
+        def calculation_and_finish(username,data_path):
+            calculation(username,data_path)
+            tk.Label(root, text="All calculations are done, close the window to finish the program.").grid(row=3)
+        tk.Button(root, text = "Press to start calculation", command = lambda: calculation_and_finish(username,data_path)).grid(row=2)
+     
+    file_path_button = tk.Button(data_frame, text = "Please choose your data file", command = lambda: openfile(username))
+    file_path_button.grid(row=0, column=0)
+    
+    tk.mainloop()
+    
+def calculation(username,data_path):
+    # Read data from data path
     original_data = pd.read_csv(data_path)
     data_array = np.nan_to_num(np.array(original_data))
 
@@ -133,14 +178,13 @@ def main():
     Nitrogen_result = np.hstack((labels, NO2_N, NO3_N))
     output_data = pd.DataFrame(Nitrogen_result)   
     output_data.columns = ['Label','Nitrite-N','Nitrate-N'] 
-    output_data.to_excel('./output/output data.xlsx', index = False)
+    output_data.to_excel('output data.xlsx', index = False)
     
     # Save the regression line pragh
-    print("All calculations are done, close the graph to finish the program.")
     plot_title = "[nitrite]:F(x)="+str(parameters[0])[:8]+"x + "+str(parameters[1])[:8]+", "+"[nitrate]:F(x)="+str(parameters[2])[:8]+"x + "+str(parameters[3])[:8]
     plt.title(plot_title)
     plt.draw()
-    plt.savefig('./output/output LR graph.png')
+    plt.savefig('output LR graph.png')
     plt.show()
-
+    
 main()
